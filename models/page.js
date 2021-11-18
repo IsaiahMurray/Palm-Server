61d06d608cc604c77c56abf49611f555b39e226e
Isaiah Murray <murray.isaiah13@gmail.com>
2021-11-17 20:55:08 -0500

    Added page model

--- models/page.js
+++ models/page.js
@@ -1,2 +1,19 @@
 const {DataTypes} = require('sequelize');
 const db = require('../db');
+
+const Page = db.define('list', {
+    ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    notebookId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
+
+module.exports = Page;
