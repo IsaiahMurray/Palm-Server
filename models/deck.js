eb5956a69131cdbda669be25014be15e683472d9
Isaiah Murray <murray.isaiah13@gmail.com>
2021-11-17 20:56:53 -0500

    Added deck model

--- models/deck.js
+++ models/deck.js
@@ -1,2 +1,19 @@
 const {DataTypes} = require('sequelize');
 const db = require('../db');
+
+const Deck = db.define('list', {
+    ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
+
+module.exports = Deck;
