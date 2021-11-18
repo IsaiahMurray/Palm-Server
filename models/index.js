200f0247cfad681793391ca64e5d91286a9f0667
Isaiah Murray <murray.isaiah13@gmail.com>
2021-11-18 10:28:49 -0500

    imported models into index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 module.exports = {
     UserModel: require("./user"),
+    CategoryModel: require("./category"),
+    ListModel: require("./list"),
+    TodoModel: require("./todo"),
+    DeckModel: require("./deck"),
+    IconModel: require("./icon"),
+    NotebookModel: require("./notebook"),
+    NotebookCategoryModel: require("./notebookCategory"),
+    NoteCardModel: require("./noteCard"),
+    PageModel: require("./page"),
+    HabitModel: require("./habit"),
   };
   
