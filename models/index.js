540261bb47fdecd11e2b938d092608e0ab384238
Isaiah Murray <murray.isaiah13@gmail.com>
2021-11-18 11:00:34 -0500

    Added many associations

--- models/index.js
+++ models/index.js
@@ -1,45 +1,74 @@
-const UserModel= require("./user"),
-const CategoryModel= require("./category"),
-const ListModel= require("./list"),
-const TodoModel= require("./todo"),
-const DeckModel= require("./deck"),
-const IconModel= require("./icon"),
-const NotebookModel= require("./notebook"),
-const NotebookCategoryModel= require("./notebookCategory"),
-const NoteCardModel= require("./noteCard"),
-const PageModel= require("./page"),
-const HabitModel= require("./habit")
+const UserModel = require("./user");
+const CategoryModel = require("./category");
+const ListModel = require("./list");
+const TodoModel = require("./todo");
+const DeckModel = require("./deck");
+const IconModel = require("./icon");
+const NotebookModel = require("./notebook");
+const NotebookCategoryModel = require("./notebookCategory");
+const NoteCardModel = require("./noteCard");
+const PageModel = require("./page");
+const HabitModel = require("./habit");
+const User = require("./user");
 
 UserModel.hasMany(ListModel, {
   as: "lists",
-  foreignKey: "ownerId"
-})
+  foreignKey: "ownerId",
+});
 ListModel.belongsTo(UserModel);
 
 ListModel.hasMany(TodoModel, {
   as: "todoItems",
-  foreignKey: "listId"
-})
+  foreignKey: "listId",
+});
 TodoModel.belongsTo(ListModel);
 
-UserModel.hasMany(TodoModel, {
-  as: "todoItems",
+// UserModel.hasMany(TodoModel, {
+//   as: "todoItems",
+//   foreignKey: "ownerId",
+// });
+// TodoModel.belongsTo(UserModel);
+
+UserModel.hasMany(HabitModel, {
+  as: "habits",
   foreignKey: "ownerId"
 })
-TodoModel.belongsTo(UserModel);
+HabitModel.belongsTo(UserModel);
 
+UserModel.hasMany(NotebookModel, {
+  as: "notebooks",
+  foreignKey: "ownerId"
+})
+NotebookModel.belongsTo(UserModel);
+
+NotebookModel.hasMany(PageModel, {
+  as: "pages",
+  foreignKey: "notebookId"
+})
+PageModel.belongsTo(NotebookModel);
+
+UserModel.hasMany(DeckModel, {
+  as: "decks",
+  foreignKey: "ownerId"
+})
+DeckModel.belongsTo(UserModel);
+
+DeckModel.hasMany(NoteCardModel, {
+  as: "noteCards",
+  foreignKey: "deckId"
+})
+NoteCardModel.belongsTo(DeckModel);
 
 module.exports = {
-    UserModel: require("./user"),
-    CategoryModel: require("./category"),
-    ListModel: require("./list"),
-    TodoModel: require("./todo"),
-    DeckModel: require("./deck"),
-    IconModel: require("./icon"),
-    NotebookModel: require("./notebook"),
-    NotebookCategoryModel: require("./notebookCategory"),
-    NoteCardModel: require("./noteCard"),
-    PageModel: require("./page"),
-    HabitModel: require("./habit")
-  };
-  
+  UserModel,
+  CategoryModel,
+  ListModel,
+  TodoModel,
+  DeckModel,
+  IconModel,
+  NotebookModel,
+  NotebookCategoryModel,
+  NoteCardModel,
+  PageModel,
+  HabitModel,
+};
