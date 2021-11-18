b6e27ba0eddf03c385fd6c474b751030c0dbf594
Isaiah Murray <murray.isaiah13@gmail.com>
2021-11-17 20:53:46 -0500

    Added notebook and notebook category model

--- models/notebook.js
+++ models/notebook.js
@@ -1,2 +1,23 @@
 const {DataTypes} = require('sequelize');
 const db = require('../db');
+
+const Notebook = db.define('list', {
+    ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    notebookCategoryId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+})
+
+module.exports = Notebook;
--- models/notebookCategory.js
+++ models/notebookCategory.js
@@ -1,2 +1,15 @@
 const {DataTypes} = require('sequelize');
 const db = require('../db');
+
+const Notebook = db.define('list', {
+    ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = Notebook;
