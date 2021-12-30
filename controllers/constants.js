e95f9acc03bebd914eb91c080bd58ed3a39e0110
Isaiah Murray <murray.isaiah13@gmail.com>
2021-12-29 22:29:02 -0500

    Added 404 const

--- controllers/constants.js
+++ controllers/constants.js
@@ -6,3 +6,16 @@ const CREATE_FAIL = "Could not be created.";
 const UPDATE_FAIL = "Could not be updated.";
 const GET_FAIL = "Could not be retrieved.";
 const DELETE_FAIL = "Could not be deleted.";
+const NOT_FOUND = "Not found."
+
+module.exports = {
+    CREATE_FAIL,
+    CREATE_SUCCESS,
+    UPDATE_FAIL,
+    UPDATE_SUCCESS,
+    DELETE_FAIL,
+    DELETE_SUCCESS,
+    GET_FAIL,
+    GET_SUCCESS,
+    NOT_FOUND
+}
