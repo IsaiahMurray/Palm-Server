708030c8f8b3d5537581f0638f0207f35c788ddc
Isaiah Murray <murray.isaiah13@gmail.com>
2021-12-29 22:13:32 -0500

    Added failureconstants

--- controllers/constants.js
+++ controllers/constants.js
@@ -2,3 +2,7 @@ const CREATE_SUCCESS = "Successfully created.";
 const UPDATE_SUCCESS = "Successfully updated.";
 const GET_SUCCESS = "Successfully retrieved.";
 const DELETE_SUCCESS = "Successfully deleted.";
+const CREATE_FAIL = "Could not be created.";
+const UPDATE_FAIL = "Could not be updated.";
+const GET_FAIL = "Could not be retrieved.";
+const DELETE_FAIL = "Could not be deleted.";
