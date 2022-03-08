44f3c92959253104b3a2cc340968aa3041d7c54a
Isaiah Murray <murray.isaiah13@gmail.com>
2022-03-08 18:58:20 -0500

    Finished most of list services

--- services/listService.js
+++ services/listService.js
@@ -1,75 +1,104 @@
 const chalk = require("chalk");
-const { ListModel } = require("../models");
+const { ListModel, TaskModel } = require("../models");
 
-const create = async({title, description, userId}) => {
-    try {
-        const newList = await ListModel.create({title, description, userId});
-        return newList
-    } catch (e) {
-        throw e;
-    }
-}
+const create = async ({ title, description, userId }) => {
+  try {
+    const newList = await ListModel.create({ title, description, userId });
+    return newList;
+  } catch (e) {
+    throw e;
+  }
+};
 
-const getaAll = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
-    }
-}
+const getaAll = async ({ userId }) => {
+  try {
+    const allLists = await ListModel.findAll({
+      where: { userId },
+      include: [{ model: TaskModel, as: "Tasks" }],
+      required: true,
+    });
 
-const getbyId = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
+    if (allLists.length === 0 || null) {
+      throw 404;
+    } else {
+      return allLists;
     }
-}
+  } catch (e) {
+    throw e;
+  }
+};
 
-const getbyTitle = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
-    }
-}
+const getbyId = async ({ id, userId }) => {
+  try {
+    const foundList = await ListModel.findOne({
+      where: { id, userId },
+      include: [{ model: TaskModel, as: "tasks" }],
+      required: true,
+    });
 
-const modify = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
+    if (foundList.length === 0 || null) {
+      throw 404;
+    } else {
+      return foundList;
     }
-}
+  } catch (e) {
+    throw e;
+  }
+};
 
-const remove = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
+const getbyTitle = async ({ title, userId }) => {
+  try {
+    const foundList = await ListModel.findAll({
+      where: { title, userId },
+      include: [{ model: TaskModel, as: "tasks" }],
+      required: true,
+    });
+    if (foundList.length === 0 || null) {
+      throw 404;
+    } else {
+      return foundList;
     }
-}
+  } catch (e) {
+    throw e;
+  }
+};
 
-const removeMany = async({}) => {
-    try {
-        
-        return
-    } catch (e) {
-        throw e;
-    }
-}
+const modify = async ({ title, description, id, userId }) => {
+  try {
+    const updatedList = await ListModel.update(
+      { title, description },
+      { where: { id, userId } }
+    );
+
+    return updatedList;
+  } catch (e) {
+    throw e;
+  }
+};
+
+const remove = async ({ id, userId }) => {
+  try {
+    const destroyedList = await ListModel.destroy({ where: { id, userId } });
+    return destroyedList;
+  } catch (e) {
+    throw e;
+  }
+};
+
+const removeMany = async ({}) => {
+  try {
+    return;
+  } catch (e) {
+    throw e;
+  }
+};
 
 module.exports = {
-    create,
-    getaAll,
-    getbyId,
-    getbyTitle,
-    modify,
-    remove,
-    removeMany
-}
+  create,
+  getaAll,
+  getbyId,
+  getbyTitle,
+  modify,
+  remove,
+  removeMany,
+};
